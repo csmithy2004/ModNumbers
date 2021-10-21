f828344972196c6f185836bfc5fb3fb569fc80c9
Connor Smith <connorsmith1979@gmail.com>
2021-10-21 09:32:48 -0500

    Add project files.

---
+++ ModNumbers/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace ModNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(161, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 215);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "It\'s A Trap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(285, 111);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(39, 42);
+            this.lblResults.TabIndex = 6;
+            this.lblResults.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
---
+++ ModNumbers/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int starter = 1;
+            int last = 100;
+            double sum = 0;
+            double alls = 0;
+            double cb = 0;
+
+            for (int i = starter; i <= last; i ++)
+            {
+                sum += i;
+                alls += Math.Pow(i, 2);
+            }
+            if(Convert.ToInt64(Math.Pow(Math.Pow(sum, 2)-alls, (1.0/3.0))) % 2 == 0)
+            {
+                
+                lblResults.Text = "Even";
+                MessageBox.Show("Sum: " + sum + " All Sums: " + alls + " Cubed Number " );
+            }
+            else
+            {
+                lblResults.Text = "Odd";
+                MessageBox.Show("Sum: " + sum + " All Sums: " + alls + " Cubed Number ");
+            }
+       
+            
+        }
+    }
+}
